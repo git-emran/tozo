dc712eb6eb80dd77ede38f63dba9d347c2d6cfe7
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-06-15 23:44:00 +0600

    feat(tozo): Integrating Pytest, sample test ran with success

---
+++ src/backend/tests/test_run.py
@@ -0,0 +1,9 @@
+import pytest
+from run import app
+
+
+@pytest.mark.asyncio
+async def test_control() -> None:
+    test_client = app.test_client()
+    response = await test_client.get("/control/ping/")
+    assert (await response.get_json())["ping"] == "pong"
