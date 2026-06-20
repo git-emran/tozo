91d2122be69b9d60f0b55e3b4657a2b8df0ad15f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-06-20 14:27:26 +0600

    feat(tozo): Ensuring rate limits

--- src/backend/src/backend/run.py
+++ src/backend/src/backend/run.py
@@ -1,10 +1,17 @@
 from quart import Quart, ResponseReturnValue
 from quart_auth import QuartAuth
+from quart_rate_limiter import RateLimitExceeded, RateLimiter, rate_exempt, rate_limit
+from quart_schema import QuartSchema
+
 from backend.lib.api_error import APIError
+from datetime import timedelta
 
 app = Quart(__name__)
 
+schema = QuartSchema(app, convert_casing=True)
+
 auth_manager = QuartAuth(app)
+rate_limiter = RateLimiter(app)
 
 
 @app.errorhandler(APIError)  # type: ignore
@@ -12,11 +19,25 @@ async def handle_api_error(error: APIError) -> ResponseReturnValue:
     return {"code": error.code}, error.status_code
 
 
+@app.errorhandler(RateLimitExceeded)  # type: ignore
+async def handle_rate_limit_exceeded_error(
+    error: RateLimitExceeded,
+) -> ResponseReturnValue:
+    return {}, 429, error.get_headers()
+
+
 @app.errorhandler(500)
 async def handle_generic_error(error: Exception) -> ResponseReturnValue:
     return {"code": "INTERNAL_SERVER_ERROR"}, 500
 
 
+@app.get("/")
+@rate_limit(6, timedelta(minutes=1))
+async def hello_handler():
+    return {"message": "hello"}
+
+
 @app.get("/control/ping/")
-async def ping() -> ResponseReturnValue:
+@rate_exempt
+async def handler():
     return {"ping": "pong"}
