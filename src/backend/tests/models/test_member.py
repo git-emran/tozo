80c71b797b8ff93fe6eb72cfbf1eefa6feaf339b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-08-06 21:58:21 +0600

    test select member by email

--- src/backend/tests/models/test_member.py
+++ src/backend/tests/models/test_member.py
@@ -4,10 +4,16 @@ from asyncpg.exceptions import UniqueViolationError
 from quart_db import Connection
 
 
-from backend.models.member import insert_member
+from backend.models.member import insert_member, select_member_by_email
 
 
 async def test_insert_member(connection: Connection) -> None:
     await insert_member(connection, "clearlybestemran@gmail.com", "")
     with pytest.raises(UniqueViolationError):
         await insert_member(connection, "Clearlybestemran@gmail.com", "")
+
+
+async def test_select_memeber_by_email(connection: Connection) -> None:
+    await insert_member(connection, "clearlybestemran@gmail.com", "")
+    member = await select_member_by_email(connection, "Clearlybestemran@gmail.com")
+    assert member is not None
